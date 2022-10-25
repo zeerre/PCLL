26df8b8ae28f61fdd9f71533763b31096fa180d8
zeerre <zeerre98988@gmail.com>
2022-10-25 12:52:43 +0800

    push to the master.comp branch

---
+++ LinuxShell/mobile.push.to.repository.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#########################################################################
+# File Name: comp.push.to.repository.sh
+# Author:GeekBear
+# mail:zeerre98988@gmail.com
+# Created And Modified Time: ====2022-10-25  12-44-45====
+#########################################################################
+
+git add .
+
+git commit -m "push to the master.mobile branch"
+
+git push origin master.mobile
+
+echo "The Works is OK!!Thanks!!!"
