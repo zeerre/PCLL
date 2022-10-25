64670838f0e9f1e61b18d6d394d18782e6d386a8
zeerre <zeerre98988@gmail.com>
2022-10-25 20:07:03 +0800

    Update the master.home branch

---
+++ C-C++/src/hello.cpp
@@ -0,0 +1,13 @@
+//*************************************************************************
+//* File Name: hello.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-10-25  19-40-47====
+//*************************************************************************
+
+#include <iostream>
+using namespace std;
+int main() {
+    printf("Hello C/C++ !!!");
+    return 0;
+}
