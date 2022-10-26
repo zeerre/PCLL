f81569de2395412370d7ac4725d9b48a5be250fa
zeerre <zeerre98988@gmail.com>
2022-10-26 08:47:05 -0400

    Update the master.fd36vm branch

---
+++ C-C++/keywd.c
@@ -0,0 +1,20 @@
+//*************************************************************************
+//* File Name: keywd.c
+//* Author:stevenzdg988
+//* Mail:steven_zdg988@sina.com 
+//* Created And Modified Time: ====2022-10-26  08-32-25====
+//*************************************************************************
+
+#include "stdio.h"
+#include <limits.h>
+int main(int argc, int argv[]){
+    printf("minimum: %d \n"
+            "maximum: %d \n"
+            "size of byte: %d \n"
+            "size of bit : %d \n",
+            INT_MIN,
+            INT_MAX,
+            sizeof(int),
+            sizeof(int)*CHAR_BIT);
+    return 0;
+}
