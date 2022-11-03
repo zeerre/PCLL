a560294cd46942ac9c2d3c1f2eeb6cffd14cde5f
zeerre <zeerre98988@gmail.com>
2022-11-03 19:12:59 +0800

    Update the master.noihomev2 branch

---
+++ LinuxShell/noihomev1.push.to.repository.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#########################################################################
+# File Name: comp.push.to.repository.sh
+# Author:GeekBear
+# mail:zeerre98988@gmail.com
+# Created And Modified Time: ====2022-10-25  12-44-45====
+#########################################################################
+
+echo "Start the Updating Remote branch from local!!!"
+echo
+echo
+
+git add .
+
+git commit -m "Update the master.noihomev1 branch"
+
+git push origin master.noihomev1
+
+echo
+echo
+echo "The Works Which push Local-repository to Remote-repository is OK!!Thanks!!!"
+
