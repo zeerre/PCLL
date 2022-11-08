dd655656ca67e81a5c23f35013930a803f869d6c
zeerre <zeerre98988@gmail.com>
2022-11-08 15:56:33 +0800

    Update the master.comp branch

---
+++ C-C++/test.cpp
@@ -0,0 +1,19 @@
+//*************************************************************************
+//* File Name: test.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-11-08  13-40-43====
+//*************************************************************************
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    while(a>0){
+        a/=2;
+        cout<<a<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
