e62521a450a8e30f6a7eb1c0481bf21ba38a801c
zeerre <zeerre98988@gmail.com>
2022-11-09 20:45:23 +0800

    Update the master.home branch

---
+++ C-C++/heapsort.h
@@ -0,0 +1,14 @@
+//*************************************************************************
+//* File Name: heapsort.h
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-11-09  20-17-19====
+//*************************************************************************
+
+#ifndef _HEAPSORT_H
+#define _HEAPSORT_H
+//#include <iostream>
+void heapify(int[],int,int);
+void heapsort(int[],int);
+void display(int[],int);
+#endif
---
+++ C-C++/heapsortht.cpp
@@ -0,0 +1,17 @@
+//*************************************************************************
+//* File Name: heapsortht.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-11-09  20-26-37====
+//*************************************************************************
+
+#include <iostream>
+#include "heapsort.h"
+using namespace std;
+int main() {
+    int a[]={90,0,32,12,21,31,12,0,56,65,76,76,87,78,98,89};
+    int len=sizeof(a)/sizeof(a[0]);
+    heapsort(a,len);
+    display(a,len);
+    return 0;
+}
