fc076d7cc96a62baa5bcc69021a70fd55cadc652
zeerre <zeerre98988@gmail.com>
2022-11-14 18:34:09 +0800

    Update the master.home branch

--- C-C++/struct.cpp
+++ C-C++/struct.cpp
@@ -8,23 +8,18 @@
 #include <iostream>
 #include<string>
 using namespace std;
-struct Students{
-    string name;
+typedef struct Students{
+    string first_name;
+    char last_name[10];
     int chinese_s;
     int math_s;
     int english_s;
     int sum_s;
 }Stu;
 int main() {
-    Students stu;
-    stu.name={"黎明"};
-    stu.chinese_s=99;
-    stu.math_s=100;
-    stu.english_s=88;
-    stu.sum_s=stu.math_s+stu.chinese_s+stu.english_s;
-    cout<<stu.sum_s<<endl;
-    Students stu1={"周华健",100,100,99};
-    stu1.sum_s=stu1.math_s+stu1.chinese_s+stu1.english_s;
-    cout<<stu1.sum_s<<endl;
+    Stu a={"周","花剑",88,99,100};
+
+    a.sum_s=a.math_s+a.chinese_s+a.english_s;
+    cout<<a.first_name<<a.last_name<<":"<<a.sum_s<<endl;
     return 0;
 }
