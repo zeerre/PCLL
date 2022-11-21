f389040c389979380daf9d0b20eae22bee06762e
zeerre <zeerre98988@gmail.com>
2022-11-21 16:27:32 +0800

    Update the master.comp branch

--- LinuxShell/shfile/emacsconfig.sh
+++ LinuxShell/shfile/emacsconfig.sh
@@ -7,6 +7,7 @@
 #########################################################################
 sudo apt-get install bear
 sudo apt-get install -y build-essential git autoconf texinfo libgnutls28-dev libxml2-dev libncurses5-dev libjansson-dev
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev gnutls-dev libgtk-3-dev
 
 git clone git://git.sv.gnu.org/emacs.git
 cd emacs
