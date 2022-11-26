e1b0786b4a75e620c9ae97b545cfc1b4191c19e7
zeerre <zeerre98988@gmail.com>
2022-11-27 01:47:35 +0800

    Update the master.home branch

--- R/plot.tg.r
+++ R/plot.tg.r
@@ -1,6 +1,6 @@
-x<- function(y) cos(y)
-y<- function(x) sin(x)
-plot(y,-pi,pi,ylim=c(-1,1),col="blue")
-plot(x,-pi,pi,ylim=c(-1,1),col="red",add=T)
+x<- function(y) cos(y)+sin(y)+log(y)
+y<- function(z) sin(z)-cos(z)
+plot(y,-pi,pi,ylim=c(-2,2),col="blue")
+plot(x,-pi,pi,ylim=c(-2,2),col="red",add=T)
 abline(h=0,v=0)
 
