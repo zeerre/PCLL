d5825ce60dd5d748a7d9ceef445d3fdd08a44a2d
zeerre <zeerre98988@gmail.com>
2022-11-30 00:42:35 +0800

    Update the master.home branch

--- LinuxShell/home.push.to.repository.sh
+++ LinuxShell/home.push.to.repository.sh
@@ -17,6 +17,6 @@ git commit -m "Update the master.home branch"
 git push origin master.home
 
 echo
-echo "UpdateTime"+$(date +%F%n%T)
+echo "UpdateTime:"-$(date +%F%n%T)
 echo "The Works Which push Local-repository to Remote-repository is COMPLETED!!Thanks!!!"
 
