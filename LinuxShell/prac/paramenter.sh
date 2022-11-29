ccba79d4a7b5668532b8f2bf2daa001515cd5058
zeerre <zeerre98988@gmail.com>
2022-11-30 02:03:35 +0800

    Update the master.home branch

---
+++ LinuxShell/prac/paramenter.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+read -p "Input paramenters:" a
+read -p "input paramenters:" b
+
+echo -e "Output paramenters: ${a},${b}"
+
+echo "Test:The script file name:${0}"
+echo "Totle paramenter numbers: $#"
+echo "The whole paramenter is : $@"
+echo "The first paramenter is : ${1}"
+echo "The second paramenter is : ${2}"
