72e1aead303d41726e8a63f4527f74253de3495d
zeerre <zeerre98988@gmail.com>
2022-11-29 23:29:02 +0800

    Update the master.home branch

---
+++ Python/loop.py
@@ -0,0 +1,15 @@
+#!bin/python
+
+num=int(input("Please Input:"))
+sum=0
+for i in range(1,num+1):
+    sum+=i
+
+print('The {} result is:{}'.format(num,sum))
+print('The {0} result is:{1}'.format(num,sum))
+print('The {num} result is:{sum}'.format(num=num,sum=sum))
+print("The result is:",sum)
+sum=sum/1.0
+print('The result is:{0:.3f}'.format(sum))
+print('The result is:%9.4f'%sum)
+print("The result is:",sum)
