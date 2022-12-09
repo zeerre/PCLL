0c4c6aac33f6c6c086d02d0241d78c56a402e70c
zeerre <zeerre98988@gmail.com>
2022-12-09 08:36:48 +0800

    Update the origin master.archcompvm branch

--- LinuxShell/opgitrepo/mobile.push.to.repository.sh
+++ LinuxShell/opgitrepo/mobile.push.to.repository.sh
@@ -19,5 +19,5 @@ git push origin master.mobile
 echo
 echo
 echo "UpdateTime:"$(date +%F%n%T)
-echo "The Works Which push Local-repository to Remote-repository is COMPLETED!!Thanks!!!"
-
+echo "The Works Which push Local-repository to \
+    origin Remote-repository is COMPLETED!!Thanks!!!"
