9ffaa0a628417d06f2bc474f79f223ab99b112be
zeerre <zeerre98988@gmail.com>
2022-12-09 08:20:38 +0800

    Update the master.archcompvm branch

---
+++ LinuxShell/opgitrepo/home.archvm.push.to.repository.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+#########################################################################
+# File Name: comp.push.to.repository.sh
+# Author:GeekBear
+# mail:zeerre98988@gmail.com
+# Created And Modified Time: ====2022-10-25  12-44-45====
+#########################################################################
+
+echo "Start the Updating Remote branch from local!!!"
+echo
+echo
+
+git add .
+
+git commit -m "Update the master.archomevm branch"
+
+git push origin master.archomevm
+
+echo
+echo "UpdateTime:"$(date +%F%n%T)
+echo "The Works Which push Local-repository to Remote-repository is COMPLETED!!Thanks!!!"
+
