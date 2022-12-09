c6248f0351f9224b8856884e504faa6587a0741e
zeerre <zeerre98988@gmail.com>
2022-12-09 08:31:17 +0800

    Update the master.archcompvm branch

--- LinuxShell/opgitrepo/comp.archvm.push.to.repository.sh
+++ LinuxShell/opgitrepo/comp.archvm.push.to.repository.sh
@@ -19,5 +19,5 @@ git push origin master.archcompvm
 echo
 echo
 echo "UpdateTime:"$(date +%F%n%T)
-echo "The Works Which push Local-repository to Remote-repository is COMPLETED!!Thanks!!!"
-
+echo "The Works Which push Local-repository to \
+    origin Remote-repository is COMPLETED!!Thanks!!!"
