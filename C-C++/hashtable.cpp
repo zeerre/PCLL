8bad455fd196a4615a6c57295e2d57f75ed3e4bf
zeerre <zeerre98988@gmail.com>
2023-03-02 14:08:42 +0800

    Update the master.comp branch

--- C-C++/hashtable.cpp
+++ C-C++/hashtable.cpp
@@ -72,7 +72,7 @@ int HashTable::getPrime(int n){
 int HashTable::hashFunction(int key){
 	return (key % capacity);
 }
-void HashTable::pass(){}
+void HashTable::pass(){cout<<"\n";}
 int main(){
     int key[] = {231, 321, 212, 321, 433, 262};
     int data[] = {123, 432, 523, 43, 423, 111};
@@ -81,6 +81,8 @@ int main(){
     for (int i = 0; i < size; i++)
         h.insertItem(key[i], data[i]);
     h.deleteItem(12);
+    h.displayHash();
+    h.pass();
     h.displayHash();
     return 0;
 }
