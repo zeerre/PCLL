b81fe9c34cce4ba7e11307b0bd6623394a2b8609
zeerre <zeerre98988@gmail.com>
2023-06-07 15:42:35 +0800

    Update the master.copmblackarch branch

--- LinuxShell/opgitrepo/home.push.to.repository.sh
+++ LinuxShell/opgitrepo/home.push.to.repository.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 #########################################################################
-# File Name: comp.push.to.repository.sh
+# File Name: home.push.to.repository.sh
 # Author:GeekBear
 # mail:zeerre98988@gmail.com
 # Created And Modified Time: ====2022-10-25  12-44-45====
